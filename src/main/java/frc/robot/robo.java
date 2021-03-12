feb7b42d81c7d15ec480037b142f98995c4f9f8f
masenn <masenn@dhs.dominicanghighschool.com>
2021-03-12 12:45:43 -0600

    init

---
+++ src/main/java/frc/robot/robo.java
@@ -0,0 +1,15 @@
+/*
+* File: robo.java
+* Author: Matthew Senn
+* Data Created: 3/12/21
+*/
+package frc.robot;
+
+import edu.wpi.first.wpilibj.drive.*;
+import edu.wpi.first.wpilibj.*;
+
+import com.revrobotics.*;
+
+public class robo extends TimedRobot {
+    
+}
