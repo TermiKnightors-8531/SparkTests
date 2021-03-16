b6be21bcf9b1681a5bad147fbfa7297305e841a0
masenn <masenn@dhs.dominicanghighschool.com>
2021-03-15 20:11:06 -0500

    +CAN ID list +tank drive +current monitor

--- src/main/java/frc/robot/robo.java
+++ src/main/java/frc/robot/robo.java
@@ -3,20 +3,23 @@ package frc.robot;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.drive.*;
 import edu.wpi.first.wpilibj.RobotDrive.*;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 import com.revrobotics.*;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class robo extends TimedRobot{
-    Joystick j = new Joystick(0);       //creates Joystick on port 0
+    XboxController xb = new XboxController(0);
     DifferentialDrive front;            //drive to control the two front motors
     DifferentialDrive rear;             //drive to control the two rear motors
     //Spark Controller Declarations
     CANSparkMax lf, lr, rf, rr;
     //CAN ID's of different 
     int lfID, lrID, rfID, rrID;
+    //Power Distribution
+    PowerDistributionPanel pdp = new PowerDistributionPanel(0);         //creates a power distribution panel with CAN ID 0
     /*
-    * Constructor for setting CAN ID's of the 
+    * Constructor for setting CAN ID's of the motor controller
     * @param lfID: CAN ID of left front motor
     * @param lrID: CAN ID of left rear  motor
     * @param rfID: CAN ID of right front motor
@@ -31,18 +34,19 @@ public class robo extends TimedRobot{
         this.rf = new CANSparkMax(rfID, MotorType.kBrushed);
         this.lr = new CANSparkMax(lrID, MotorType.kBrushed);
         this.rr = new CANSparkMax(rrID, MotorType.kBrushed);
-        front = new DifferentialDrive(lf, rf);
-        rear = new DifferentialDrive(lr, rr);
+        front = new DifferentialDrive(this.lf, this.rf);
+        rear = new DifferentialDrive(this.lr, this.rr);
     }
     
     @Override
     public void teleopPeriodic(){
-        adrive();                    
+        syncDrive();                    
     }
 
     //functions to drive both front and rear motors at the same time
-    public void adrive(){
-        front.arcadeDrive(j.getY(), j.getX());
-        rear.arcadeDrive(j.getY(), j.getX());
+    public void syncDrive(){
+        front.tankDrive(xb.getY(Hand.kLeft), xb.getY(Hand.kRight));
+        rear.tankDrive(xb.getY(Hand.kLeft), xb.getY(Hand.kRight));
+        System.out.println(pdp.getCurrent(0));          //reads current from PDP channel 0
     }
 }
