9a85d7b89e24433bc489303d7924bd8aaa2316b6
masenn <masenn@dhs.dominicanhighschool.com>
2021-03-16 12:46:37 -0500

    Added drive train functionality

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -1,29 +1,39 @@
 package frc.robot;
 
-import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 
-public class DriveTrain {
-    //class to hold the two motor controllers for a single side
-    side left, right;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
-    //TODO Evaluate necesity (I may not need this class/it may be redundant)
-    //I am just trying to increase the verbosity of the code
-    class motor extends CANSparkMax {
-        private int deviceID, pChan;
-        public motor(int deviceID, MotorType type, int pChan) {
-            super(deviceID, type);
-            this.deviceID = deviceID;
-            this.pChan = pChan;
-        }
-        
+
+public class DriveTrain {
+    //instances of side for both left and right side
+    private side left, right; 
+
+    //constructor with two sides 
+    public DriveTrain(side left, side right){
+        this.left = left;
+        this.right = right;
+    }
+
+    //constructor to create two sides from 4 IDs
+    public DriveTrain(int lf, int lr, int rf, int rr){
+        this.left = new side(lf, lr);
+        this.right = new side(rf, rr);
     }
     
+    //function to drive robot based on the left and right joystick
+    public void drive(XboxController xb){
+        this.left.drive(xb.getY(Hand.kLeft));
+        this.right.drive(xb.getY(Hand.kRight));
+    }
+
+    //class to represent a single side (two motors per wheel)
     class side {
         private int fID,rID;                                //ints to hold the CAN ID's
         private CANSparkMax front,rear;                     //motor controller instances
-        private int fchan, rchan;
+        private int fchan, rchan;                           //possible ints for holding PDP channels for motor controllers
 
         //constructor with CAN IDs
         public side(int fID, int rID){
@@ -33,22 +43,12 @@ public class DriveTrain {
             rear =  new CANSparkMax(this.rID, MotorType.kBrushed);
         }
         
-        //constructor with CAN IDs and power channels from the PDP (for monitoring current output)
-        public side(int fID, int rID, int fchan, int rchan){
-            this.fID = fID;
-            this.rID = rID;
-            this.fchan = fchan;
-            this.rchan = rchan;
-            front = new CANSparkMax(this.fID, MotorType.kBrushed);
-            rear =  new CANSparkMax(this.rID, MotorType.kBrushed);
-        }
-
         //function to drive the two motors at the same speed
         public void drive(double speed) {
             front.set(speed);
             rear.set(speed);
         }
 
-
     }
+
 }
