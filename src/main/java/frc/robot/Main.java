928dfa498d5c1c56b3eab7c4799efe5f350eac61
masenn <masenn@dhs.dominicanhighschool.com>
2021-03-17 13:29:28 -0500

    added comments

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -12,10 +12,17 @@ public final class Main {
   private Main() {}
 
   public static void main(String... args) {
-    //costructor for robot class
+    //specifices a drive train with motor controller IDs 1 and 2 for the left side motors and 3 and 4 for the right side
     DriveTrain dt = new DriveTrain(1,2,3,4);
+
+    //Creates an arm with a CAN ID of the 123 for the arm motor and a CAN ID of 124 for the intake motor
+    //upwards limit switch created on Digital Input/Output port 0 and downwards limit switch created on Digital Input/Output port 1 
     Arm a = new Arm(123, 124, new DigitalInput(0), new DigitalInput(1));
+   
+    //Creates an XbobxController on port 0
     XboxController xb = new XboxController(0);
+
+    //starts the robot with dt as the drive train, arm a, and XboxController xb
     RobotBase.startRobot(() -> new RobotAssembly(dt, a, xb));
   }
 }
