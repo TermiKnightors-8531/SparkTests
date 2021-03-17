61646dfa05557d0fe4fbd97f790e22038ec40f4b
masenn <masenn@dhs.dominicanhighschool.com>
2021-03-17 14:23:33 -0700

    Added autonomous init and configure

--- src/main/java/frc/robot/RobotAssembly.java
+++ src/main/java/frc/robot/RobotAssembly.java
@@ -2,18 +2,31 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class RobotAssembly extends TimedRobot{
-    // TODO implement Arm
     private  Arm a;
     private XboxController xb;
     private DriveTrain dt;
-    private PowerDistributionPanel pdp = new PowerDistributionPanel(0);
+    //private PowerDistributionPanel pdp = new PowerDistributionPanel(0);
+    private Timer timer = new Timer();
 
     public RobotAssembly (DriveTrain dt, Arm a, XboxController xb){
         this.dt = dt;
         this.xb = xb;
+    }
+
+    //TEMP
+    @Override
+    public void autonomousInit() {
+
+    }
+    
+    //TEMP motor test
+    @Override
+    public void autonomousPeriodic() {
+        a.test_arm();
     }
 
     @Override
@@ -23,6 +36,7 @@ public class RobotAssembly extends TimedRobot{
 
     public void teleopPeriodic() {
         this.dt.drive(xb);
+
 
     }
 
