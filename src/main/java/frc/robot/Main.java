959975cf0e111a1a2836ef890c48bfcb50934b2f
masenn <masenn@dhs.dominicanghighschool.com>
2021-03-22 12:22:37 -0500

    added PowerDistrubutionPanel

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -6,11 +6,16 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController;
 
 public final class Main {
   private Main() {}
+
+  //creates a publically accesable Power Distribution Panel for monitoring current draw on motors
+  //also can read temperature and battery voltage
+  public PowerDistributionPanel pdp = new PowerDistributionPanel(0);
 
   public static void main(String... args) {
     //specifices a drive train with motor controller IDs 1 and 2 for the left side motors and 3 and 4 for the right side
@@ -20,7 +25,7 @@ public final class Main {
     //upwards limit switch created on Digital Input/Output port 0 and downwards limit switch created on Digital Input/Output port 1 
     Arm a = new Arm(10, 11, new DigitalInput(0), new DigitalInput(1));
    
-    //Creates an XbobxController on port 0
+    //Creates an XbobxController on port 0 as seen in the driver station
     XboxController xb = new XboxController(0);
 
     //starts the robot with dt as the drive train, arm a, and XboxController xb
