0e484066001ec59a1b50ec262ff6c42e04212592
masenn <masenn@dhs.dominicanghighschool.com>
2021-03-22 13:20:27 -0500

    Added constructor for current monitoring

--- src/main/java/frc/robot/Arm.java
+++ src/main/java/frc/robot/Arm.java
@@ -4,14 +4,18 @@ import edu.wpi.first.wpilibj.DigitalInput;
 
 import com.ctre.phoenix.motorcontrol.can.*;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode.*;
 
 public class Arm {
     int armID,intakeID;
+    //TODO determine correct limit switch
     DigitalInput upSwitch, downSwitch;
     VictorSPX armMotor,intakeMotor;
-
+    //PowerDistribution Panel Channels for monitoring current
+    private int armChannel, intakeChannel;
     private double intakeSpeed = .5;
+
     //constructor for creating an arm with ID's for VictorSPX controller
     public Arm (int armID, int intakeID, DigitalInput upSwitch, DigitalInput downSwitch){
         this.armID = armID;
@@ -20,7 +24,41 @@ public class Arm {
         this.downSwitch = downSwitch;
         this.armMotor = new VictorSPX(armID);
         this.intakeMotor = new VictorSPX(intakeID);
+        armMotor.configFactoryDefault();
+        armMotor.setNeutralMode(NeutralMode.Brake);
+        intakeMotor.configFactoryDefault();
+        intakeMotor.setNeutralMode(NeutralMode.Brake);
+
     }
+
+    /*constructor for creating an arm with ID's for VictorSPX controller and PowerDistributionPanel channels
+    * @param armID: the CAN ID of the arm
+    * @param intakeID: the CAN ID of the intake motor
+    * @param armChannel: the channel of the motor controller for the arm 
+    * @param intakeChannel: the channel of the motor 
+    * @param brake: if true: brake on idle / if false: coast on idle
+    * @param intakeSpeed: speed of arm 
+    */ 
+    public Arm (int armID, int intakeID, int armChannel, int intakeChannel, boolean brake, double intakeSpeed){
+        this.armID = armID;
+        this.intakeID = intakeID;
+        this.armMotor = new VictorSPX(armID);
+        this.intakeMotor = new VictorSPX(intakeID);
+        this.armChannel = armChannel;
+        this.intakeChannel = intakeChannel;
+        armMotor.configFactoryDefault();
+        intakeMotor.configFactoryDefault();
+        if (brake) {
+            armMotor.setNeutralMode(NeutralMode.Brake);
+            intakeMotor.setNeutralMode(NeutralMode.Brake);
+        }
+        else {
+            armMotor.setNeutralMode(NeutralMode.Coast);
+            intakeMotor.setNeutralMode(NeutralMode.Coast);
+        }
+        this.intakeSpeed = intakeSpeed;
+    }
+
 
     //method to raise arm while limit switch has not been pressed
     public void raiseArm () {
